9bdfd067cee5b2efc4d473620de5ace1f29aa70b
obirikan <obirikan020@gmail.com>
2023-01-14 08:18:47 +0000

    first commit

---
+++ screens/Intro.jsx
@@ -0,0 +1,21 @@
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <View>
+      <Text>Intro</Text>
+    </View>
+  )
+}
+
+export default Intro
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
